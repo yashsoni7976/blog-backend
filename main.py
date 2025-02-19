112abba1d679d563dc9a226c2b1fe052a5e42b73
yashsoni7976 <yashsoni7976@gmail.com>
2025-02-19 17:56:42 +0530

    Refactor main.py to improve code organization by removing commented sections and ensuring proper router inclusion for user, blog, and auth routes.

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ from src.models import user_model, auth_model, blog_model
 from src.router import user_router, auth_router, blog_router
 from src.middleware.auth_middleware import get_current_user
 
-# Load environment variables from the .env file
 
-# Create tables in the database
 user_model.Base.metadata.create_all(bind=engine)
 auth_model.Base.metadata.create_all(bind=engine)
 blog_model.Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI()
 
 
-# Include routers for the user and auth routes
 app.include_router(user_router.router, prefix="/api/v1/user", tags=["user"], dependencies=[Depends(get_current_user)])
 app.include_router(blog_router.router, prefix="/api/v1/blog", tags=["blog"], dependencies=[Depends(get_current_user)])
 app.include_router(auth_router.router, prefix="/api/v1/auth", tags=["auth"])
+
 
 @app.get("/")
 def read_root():
